ed419fcc2968f5dd43c5ac8bc077f201863552df
Tejo Kumar Reddy Chenchu <tejo.kumar.reddy.chenchu@sap.com>
2015-05-06 22:26:04 -0700

    Check if product is in cart. Server side check.

--- app/controllers/products.server.controller.js
+++ app/controllers/products.server.controller.js
@@ -1,6 +1,10 @@
 'use strict';
-var http = require('http');
-var bestBuyKey = '7p7nhhmq6ftt22347pmcz5dt';
+var http = require('http'),
+    bestBuyKey = '7p7nhhmq6ftt22347pmcz5dt',
+    mongoose = require('mongoose'),
+    errorHandler = require('./errors.server.controller'),
+    Cart = mongoose.model('Cart'),
+    _ = require('lodash');
 exports.list = function(req,res){
 	var searchString = 'iPhone';
 	if(req.query && req.query.searchText){
@@ -20,7 +24,27 @@ exports.list = function(req,res){
 		});
 		dataResponse.on('end',function(){
 			var jsonDataResponse = JSON.parse(responseString);
-			res.json(jsonDataResponse.products);
+            if(jsonDataResponse.products){
+                var products = jsonDataResponse.products;
+                Cart.find().sort('-created').exec(function(err,carts){
+                    if(!err && carts){
+                        var cart = carts[0];
+                        var cartProducts = cart.products;
+                        _.forEach(cartProducts,function(cartProduct){
+                            var product = _.find(products,function(prdct){
+                                return prdct.sku === cartProduct.sku;
+                            });
+                            if(product){
+                                product.isInCart = true;
+                            }
+                        });
+                    }
+                    res.json(products);
+                });
+            }else {
+                res.json([]);
+            }
+
 		});
 	}).end();
 };
